e953318de17e1d6f97ce9a28337bf83c06c2fe84
Ikhsan <dev.ikhsanmohamad@gmail.com>
2023-05-23 17:17:11 +0700

    feat: add base layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,153 +1,20 @@
-import React, { FC } from "react";
-import Head from "next/head";
-import { useRouter } from "next/router";
+import type { FC } from "react";
 import useTranslation from "next-translate/useTranslation";
-import setLanguage from "next-translate/setLanguage";
 
-import {
-    Box,
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    Container,
-    Grid,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-} from "@material-ui/core";
-import { Check } from "@material-ui/icons";
-import { ROUTES_PATH } from "@constants/config";
+// Components
+import { BaseLayout } from "@components/layouts";
 
-const Index: FC = () => {
-    const { t } = useTranslation();
-    const route = useRouter();
-    const { locale } = route;
+const Home: FC = () => {
+  const { t } = useTranslation();
 
-    const tempRequirements = [
-        { desc: "requirement-desc-1", action: null },
-        { desc: "requirement-desc-2", action: "change-language" },
-        { desc: "requirement-desc-3", action: null },
-        { desc: "requirement-desc-4", action: null },
-        { desc: "requirement-desc-5", action: null },
-    ];
-
-    const handleChangeLanguage = async () => {
-        await setLanguage(locale === "id" ? "en" : "id");
-    };
-
-    return (
-        <>
-            <Head>
-                <title>REY - Project Test</title>
-            </Head>
-            <Container maxWidth="xl">
-                <Box component="div" m={10}>
-                    <Grid container>
-                        <Grid container justifyContent="center" item xs={12}>
-                            <Typography
-                                align="center"
-                                variant="h4"
-                                component="h3"
-                            >
-                                {t("home:welcome-title")}
-                            </Typography>
-                        </Grid>
-                        <Grid container justifyContent="center" item xs={12}>
-                            <Box component="div" m={2}>
-                                <Container maxWidth="sm">
-                                    <Typography variant="body1" component="p">
-                                        {t("home:welcome-description")}
-                                        <Button
-                                            target="_blank"
-                                            href="https://pokeapi.co/"
-                                            color="primary"
-                                            style={{
-                                                color: "##0082a3",
-                                                fontWeight: "bold",
-                                                textTransform: "capitalize",
-                                            }}
-                                        >
-                                            Pokemon API
-                                        </Button>
-                                    </Typography>
-                                    <Box m="5" height={30} />
-                                    <Card variant="elevation" elevation={8}>
-                                        <CardContent>
-                                            <Typography
-                                                color="textPrimary"
-                                                variant="h6"
-                                                gutterBottom
-                                            >
-                                                {t("home:requirement-title")}
-                                            </Typography>
-                                            <List>
-                                                {tempRequirements.map(
-                                                    (requirement, key) => (
-                                                        <ListItem
-                                                            disableGutters
-                                                            key={`requirement-list-${key}`}
-                                                        >
-                                                            <ListItemIcon>
-                                                                <Check />
-                                                            </ListItemIcon>
-                                                            <ListItemText>
-                                                                {t(
-                                                                    `home:${requirement.desc}`,
-                                                                )}
-                                                                {requirement.action ===
-                                                                    "change-language" && (
-                                                                    <Button
-                                                                        variant="contained"
-                                                                        color="primary"
-                                                                        size="small"
-                                                                        onClick={
-                                                                            handleChangeLanguage
-                                                                        }
-                                                                    >
-                                                                        {t(
-                                                                            `common:language-${locale}`,
-                                                                        )}
-                                                                    </Button>
-                                                                )}
-                                                            </ListItemText>
-                                                        </ListItem>
-                                                    ),
-                                                )}
-                                            </List>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button
-                                                variant="contained"
-                                                color="primary"
-                                                fullWidth
-                                                onClick={() =>
-                                                    route.push(
-                                                        ROUTES_PATH.pokemon_list,
-                                                    )
-                                                }
-                                            >
-                                                {t("home:requirement-action")}
-                                            </Button>
-                                        </CardActions>
-                                    </Card>
-                                </Container>
-                            </Box>
-                        </Grid>
-                        <Grid container justifyContent="center" item xs={12}>
-                            <Box component="div" m={5}>
-                                <Typography variant="h4" component="h3">
-                                    {t("home:welcome-work")}
-                                </Typography>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Container>
-        </>
-    );
+  return (
+    <BaseLayout>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione culpa
+      aperiam nisi? Dolore laboriosam at natus maxime odit. Non corporis
+      voluptatibus debitis temporibus earum voluptatum obcaecati amet sed quo
+      repellat!
+    </BaseLayout>
+  );
 };
 
-export default Index;
+export default Home;
---
+++ src/components/layouts/BaseLayout.tsx
@@ -0,0 +1,41 @@
+import { memo } from "react";
+import Head from "next/head";
+
+// Types
+import type { NextPage } from "next";
+import { Header } from "@components/organisms";
+import { Container } from "@mui/material";
+
+export interface BaseLayoutProps {
+  title?: string;
+  description?: string;
+  content?: string;
+  icon?: string;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+// eslint-disable-next-line react/display-name
+export const BaseLayout: NextPage<BaseLayoutProps> = memo((props) => {
+  const { title = "Pokemon", description, content, icon, children } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name={description ?? "Kunci Dashboard"}
+          content={content ?? "Kunci Dashboard"}
+        />
+        <link rel="icon" href={icon ?? "/favicon.ico"} />
+      </Head>
+      <main>
+        <Container maxWidth="lg">
+          <Header />
+
+          {children}
+        </Container>
+      </main>
+    </>
+  );
+});
