62fa54f9c24453f760f8526ebd34d560e0f011b4
Ikhsan <dev.ikhsanmohamad@gmail.com>
2023-05-23 14:57:34 +0700

    feat: setup custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/styles";
 
+// Styles
+import { theme } from "@styles/theme";
 import "@styles/global.css";
 import "@styles/vars.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return (
-        <Component {...pageProps} />
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    warning: {
+      main: "#E6AB09",
+      light: "#FFAF66",
+      dark: "#E66D00",
+    },
+  },
+});
